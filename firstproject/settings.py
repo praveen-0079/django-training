5ef5e51527476e69f6b132742548a4aa707413ec
praveen-0079 <praveenyadav2322@gmail.com>
2024-10-08 13:56:09 +0530

    Phas 1

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'news.apps.NewsConfig',
-    'polls',
 ]
 
 MIDDLEWARE = [
@@ -108,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Kolkata'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -119,8 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- firstproject/urls.py
+++ firstproject/urls.py
@@ -17,15 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from news import views
-from django.conf import settings
-from django.conf.urls.static import static
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home),
-    path("articles/", include("news.urls")),
-    path("polls/", include("polls.urls"))
+    path("articles/", include("news.urls"))
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
